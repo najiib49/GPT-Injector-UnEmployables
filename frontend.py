0ebd386c7888adf6ab4b4ef6bb08d51565554f27
Najiib <maxamednajiib10@hotmail.com>
2023-01-07 23:24:33 -0700

    frontend incorporates xss checker

--- frontend.py
+++ frontend.py
@@ -1,6 +1,9 @@
 import streamlit as st
 # import pandas as pd
 # import numpy as np
+import scanner as sc
+from requests.exceptions import MissingSchema
+
 
 st.title('GPT-Injector ')
 
@@ -9,3 +12,14 @@ user_input = st.text_input(
         key="placeholder",
         placeholder="This is placeholder",
     )
+try:
+    result = sc.xss_checker(user_input)
+    if not result:
+        # st.write('Hello, *World!* ')
+        st.write("XSS vulneribility discovered!")
+        # print("Attack payload:"+payload)
+    else:
+        st.write("Secure :sunglasses:")
+except:
+    st.write("PLEASE ENTER A URL : 😀 ")
+
